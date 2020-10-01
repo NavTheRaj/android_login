ef710f4d0a0c928a13fa87a3384a08d3e5cbf15a
NavTheRaj <navrajkhanal61@gmail.com>
2020-10-01 13:42:12 +0545

    Added radiogroup functions

--- app/src/main/java/com/example/loginform/WelcomeActivity.java
+++ app/src/main/java/com/example/loginform/WelcomeActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -24,14 +26,30 @@ public class WelcomeActivity extends AppCompatActivity {
     public void messagePass(View view){
         EditText editMessage;
         String message;
+        String gender;
 
         editMessage = (EditText)findViewById(R.id.message);
+
+        gender = getSelecetedGender();
 
         message = editMessage.getText().toString();
 
         Intent resultIntent = new Intent();
-        resultIntent.putExtra(MainActivity.USERNAME,message);
+        resultIntent.putExtra(MainActivity.USERNAME,message+"|"+gender);
         setResult(RESULT_OK,resultIntent);
         finish();
+    }
+
+    public String getSelecetedGender(){
+        RadioGroup radioGroup;
+        RadioButton radioButton;
+        radioGroup = (RadioGroup) findViewById(R.id.gender);
+
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+
+        radioButton = (RadioButton) findViewById(selectedId);
+
+        return radioButton.getText().toString();
+
     }
 }
